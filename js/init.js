ac5266c0f3eb8da8d9f8438a32aaef19d4cc43b1
Kloven <jloup.m@gmail.com>
2019-11-27 17:19:44 +0100

    refacto

--- js/init.js
+++
@@ -1,9 +0,0 @@
-var canvas, engine ;
-
-function init(){
-  var canvas = document.getElementById("renderCanvas") ; 
-  var engine = new BABYLON.Engine(canvas,true) ; 
-  var scene  = createScene() ; 
-  engine.runRenderLoop(function(){scene.render();}) ; 
-  window.addEventListener('resize',function(){engine.resize();}) ; 
-}
