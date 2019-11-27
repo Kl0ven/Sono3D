b1da105a96ee3c096f478c5f2f36cb7b6a4e3a05
Kloven <jloup.m@gmail.com>
2019-11-27 15:49:10 +0100

    hotfix

--- js/utils.js
+++ js/utils.js
@@ -79,9 +79,9 @@ function allSoundReady(){
 
 function playSeq(mesh, type) {
 	if(mesh.metadata !== null && mesh.metadata.hasOwnProperty(type) && !mesh.metadata[type].isPlaying()){
-		mesh.metadata[type].play(type.split("_")[1])
+		mesh.metadata[type].play(type === "classic" ? type : type.split("_")[1] )
 	}else if (mesh.parent !== null && mesh.parent.metadata !== null && mesh.parent.metadata.hasOwnProperty(type) && !mesh.parent.metadata[type].isPlaying()) {
-		mesh.parent.metadata[type].play(type.split("_")[1])
+		mesh.parent.metadata[type].play(type === "classic" ? type : type.split("_")[1] )
 	}
 }
 
